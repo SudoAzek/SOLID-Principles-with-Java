bae02b42bd554a7a8cbfa265f032650fc52d779a
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-07 07:42:05 +0900

    LSP solution for MediaPlayer example
    Liskov Substitution Principle solution example with a media player functionality.

---
+++ src/com/mezonworks/solid/lsp/mediaplayer_lsp_solution/TorVideoPlayer.java
@@ -0,0 +1,4 @@
+package com.mezonworks.solid.lsp.mediaplayer_lsp_solution;
+
+public class TorVideoPlayer extends VideoMediaPlayer {
+}
---
+++ src/com/mezonworks/solid/lsp/mediaplayer_lsp_solution/VideoMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.mezonworks.solid.lsp.mediaplayer_lsp_solution;
+
+public class VideoMediaPlayer extends MediaPlayer {
+    public void PlayVideo() {
+        System.out.println("Playing video...");
+    }
+}

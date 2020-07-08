48faf2841815a92de92c1312417501a004cd4b33
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-08 11:43:16 +0900

    ISP violation example

---
+++ src/com/mezonworks/solid/isp/bird_isp_violation/Strauss.java
@@ -0,0 +1,18 @@
+package com.mezonworks.solid.isp.bird_isp_violation;
+
+public class Strauss implements Bird {
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void run() {
+        // Realization
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
---
+++ src/com/mezonworks/solid/isp/bird_isp_violation/Swallow.java
@@ -0,0 +1,18 @@
+package com.mezonworks.solid.isp.bird_isp_violation;
+
+public class Swallow implements Bird {
+    @Override
+    public void fly() {
+        // Realization
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
